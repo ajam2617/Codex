f2b033754e01666d3553050f6fe845597eafb404
jrmoore117 <jrmoore117@gmail.com>
2019-01-08 14:39:23 -0800

    Set up controller router with test route.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,18 @@
-app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
+// Create router
+var express = require("express");
+var router = express.Router();
+
+var models = require("../models");
+
+// Connect to sequelize model
+var sequelizeConnection = models.sequelize;
+sequelizeConnection.sync();
+
+router.get("/", function (req, res) {
+
+   console.log("success!");
+
+   // res.render("index", handlebarsObj);
+})
+
+module.exports = router;
