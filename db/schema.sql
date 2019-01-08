c78bf4b4b8db8fa52c24ebb6df74754f849b2f0b
jrmoore117 <jrmoore117@gmail.com>
2019-01-08 14:06:53 -0800

    Add sequel database to schema.sql. Add sequelize tables to model.js.

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS `codex_db`;
+CREATE DATABASE `codex_db`;
---
+++ models/model.js
@@ -0,0 +1,69 @@
+module.exports = function(sequelize, DataTypes) {
+
+   var tables = {
+      Users: sequelize.define("Users", {
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+        firstName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+        lastName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        }
+      }),
+      Snippets: sequelize.define("Snippets", {
+         snippet: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+               len: [1]
+            }
+         },
+         language: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+               len: [1]
+            }
+         },
+         tags: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+               len: [1]
+            }
+         },
+         description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+               len: [1]
+            }
+         },
+         lastUpdate: Sequelize.DATE
+      })
+   }
+
+   return tables;
+ };
+ 
